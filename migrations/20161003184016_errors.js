2bd5a703d3b6a1366dd9dfcffbd764098bbeb92c
mtcrouse <mettacrouse@gmail.com>
2016-10-04 23:31:54 -0700

    fix foreign key

--- migrations/20161003184016_errors.js
+++ migrations/20161003184016_errors.js
@@ -3,6 +3,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('errors', (table) => {
     table.increments();
+    table.foreign('problem_id').references('problems.id');
     table.text('type').notNullable();
     table.timestamps(true, true);
   });
