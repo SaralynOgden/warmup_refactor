950d3d4de5440a2d1b9b83d6dbd37e82db52f13c
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-10-05 19:44:39 -0700

    Corrected the migrations files and tested them

--- migrations/20161005164659_problem_status.js
+++ migrations/20161005164659_problem_status.js
@@ -2,11 +2,11 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('problem_status', (t) => {
-    t.increments();
-    t.foreign('user_id').references(users.id);
-    t.foreign('problem_id').references(problems.id);
+    t.increments().primary();
+    t.integer('user_id').unsigned().references('id').inTable('users');
+    t.integer('problem_id').unsigned().references('id').inTable('problems');
     t.enum('progress', ['UNATTEMPTED', 'STARTED', 'ATTEMPTED', 'SOLVED']).defaultTo('UNATTEMPTED');
-    t.specificType('hashed_password').notNullable();
+    t.specificType('hashed_password', 'char(60)').notNullable();
     t.timestamps(true, true);
   });
 };
--- migrations/20161005165337_cohorts_users.js
+++ migrations/20161005165337_cohorts_users.js
@@ -1,16 +1,15 @@
 'use strict'
 
 exports.up = function(knex) {
-  return knex.schema.createTable('problem_status', (t) => {
+  return knex.schema.createTable('cohorts_users', (t) => {
     t.increments();
-    t.foreign('cohort_id').references(cohorts.id);
-    t.foreign('user_id').references(users.id);
+    t.integer('cohort_id').unsigned().references('id').inTable('cohorts');
+    t.integer('user_id').unsigned().references('id').inTable('users');
     t.enum('progress', ['UNATTEMPTED', 'STARTED', 'ATTEMPTED', 'SOLVED']).defaultTo('UNATTEMPTED');
-    t.specificType('hashed_password').notNullable();
     t.timestamps(true, true);
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('problem_status');
+  return knex.schema.dropTable('cohorts_users');
 };
